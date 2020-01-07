755203fdf9876812cc7b3ca5172a02531df9e708
Max Kelly <max@revelthat.com>
2020-01-07 12:00:43 +1100

    added redux to project

--- src/counter.js
+++ src/counter.js
@@ -1,23 +1,42 @@
 import React from 'react';
+import { connect } from 'react-redux';
 
 // Imports CSS Document
 import './counter.css';
 
+function mapStateToProps( state ) {
+  return { count: state.count };
+}
 class Counter extends React.Component {
+
+  sendToStore (count) {
+    this.props.dispatch({ type: "UPDATE_COUNT", newCount: count })
+  }
+  // The dispatch says create an action object and I will then send it to the reducer on index.js.
+  // The type allows us to link the the types to the reducer
+  increment = () => {
+    this.sendToStore( this.props.count + 1 ); 
+  };
+
+  // This takes 1 away 
+  decrement = () => {
+    this.sendToStore( this.props.count - 1 ); 
+  };
 
   // This re renders thr app as well as adds the buttons etc... in 
   render(){
-    return(
+    return (
       <div className="counter">
         <h2> Counter </h2>
         <div>
-          <button onClick={this.props.decrement}> - </button>
+          {/* Because we have the dispatch above we only need to call 'this.functionName' */}
+          <button onClick={this.decrement}> - </button>
           <span className="count">{this.props.count}</span>
-          <button onClick={this.props.increment}> + </button>
+          <button onClick={this.increment}> + </button>
         </div>
       </div>
     )
   }
 }
 
-export default Counter
+export default connect( mapStateToProps )( Counter );
--- src/index.js
+++ src/index.js
@@ -1,44 +1,56 @@
 import React from "react";
 import { render } from "react-dom";
-import Counter from './counter.js';
+// This creates a redux
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
 
+// Other files 
+import Counter from './counter.js';
 
 // Import CSS
 import "./index.css";
 import SomeContainer from "./someContainer.js";
 
+// This defines an initial state incase redux is undefined
+const initialState = { count: 0 };
+
 class App extends React.Component {
 
-  state = { count: 0 };
+  // The state = initialState basically defines it as 0 instead of undefined 
+  reducer( state = initialState, action ) {
 
-  increment = () => {
-    this.setState({ count: this.state.count + 10000 })
-  }
+    // The newState should never be modified within the reducer 
+    let newState = {};
 
-  // This takes 1 away 
-  decrement = () => {
-    this.setState({ count: this.state.count - 10000 })
-  }
+    // Switch is just like case in Ruby.
+    switch( action.type ) {
+      case "UPDATE_COUNT":
+        newState = { count: action.newCount };
+        break;
+
+      default:
+        newState = { ...state };
+    }
+    
+    return newState
+  };
+
+  // Store is where we hold the data and actions redux 
+  store = createStore( this.reducer );
 
   render () {
     return (
-      <div>
-        <div className="arbitrary-block">
-          {/* The below renders the counter file */}
-          < Counter 
-            count={this.state.count}
-            increment={this.increment}
-            decrement={this.decrement}
-          />
+      <Provider store={this.store}>
+        <div>
+          <div className="arbitrary-block">
+            {/* The below renders the counter file */}
+            < Counter />
+          </div>
+          <div className="another-arbitrary-block">
+            < SomeContainer />
+          </div>
         </div>
-        <div className="another-arbitrary-block">
-          < SomeContainer 
-            count={this.state.count}
-            increment={this.increment}
-            decrement={this.decrement}
-          />
-        </div>
-      </div>
+      </Provider>
     )
   }
 }
